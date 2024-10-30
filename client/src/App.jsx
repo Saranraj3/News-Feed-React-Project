d68616a98b5e43b8a435401fa95edfd61fdbc563
Saranraj <www.saranrajcoder@gmail.com>
2024-10-30 13:48:26 +0530

    Some Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Signup from './Authentication/Signup'
-import Login from './Authentication/Login'
+import React from "react";
+import { Route, Routes, BrowserRouter} from "react-router-dom";
+import Signup from "./Authentication/Signup";
+import Login from "./Authentication/Login";
 
 const App = () => {
   return (
-    <div>
-      <Signup/>
-      <Login/>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- client/src/Authentication/Login.jsx
+++ client/src/Authentication/Login.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
     <div>
-      Login
+      <input
+        className="border border-black"
+        type="text"
+        placeholder="First Name"
+      />
+      <br />
+      <br />
+      <input
+        className="border border-black"
+        type="text"
+        placeholder="Last Name"
+      />
+      <br />
+      <br />
+      <input className="border border-black" type="email" placeholder="Email" />
+      <br />
+      <br />
+      <input
+        className="border border-black"
+        type="text"
+        placeholder="Address"
+      />
+      <br />
+      <br />
+      <input
+        className="border border-black"
+        type="text"
+        placeholder="Nationality"
+      />
+      <br />
+      <br />
+      <div className="flex">
+        <div className="flex">
+          <p>Male</p>
+          <input type="radio" name="radAnswer" />
+        </div>
+        <div className="flex">
+          <p>Female</p>
+          <input type="radio" name="radAnswer" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
